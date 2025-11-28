efdb473089cfe24b602f98787ac5ee1665d1b442
Weii Wang <weii.wang@canonical.com>
2025-11-28 15:10:13 +0800

    Update broken link

--- docs/conf.py
+++ docs/conf.py
@@ -249,7 +249,8 @@ redirects = {
 
 linkcheck_ignore = [
     "http://127.0.0.1:8000",
-    "https://github.com/canonical/ACME/*"
+    "https://github.com/canonical/ACME/*",
+    "https://matrix.to/#/#charmhub-charmdev:ubuntu.com",
     "https://matrix.to/#/#charmhub-charmdev:ubuntu.com"
     ]
 
